462ddb1ce0f95519ac48f9f529522521e2d56ded
So <1979510177@qq.com>
2016-11-22 18:46:14 +0800

    use vue-router add async load components,complete a simple demo

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,28 @@
   <div id="app">
     <div>{{ count }}</div>
     <button @click="add">add</button>
+    <h1>Hello App!</h1>
+    <p>
+      <!-- 使用 router-link 组件来导航. -->
+      <!-- 通过传入 `to` 属性指定链接. -->
+      <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+      <router-link to="/hello">Go to hello</router-link>
+    </p>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
   </div>
+
 </template>
 
 <script>
 import { mapState , mapActions } from 'vuex'
 import store from './store/index'
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
 
 export default {
   name: 'app',
@@ -35,5 +50,10 @@ export default {
 </script>
 
 <style>
-
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App'
 import store from './store/index'
-
+import router from './router/index'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   store,  //从根组件全局注入store
+  router,
   template: '<App/>',
   components: { App }
 })
